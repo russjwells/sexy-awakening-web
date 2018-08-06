13fe3901c449e01de5cf7a90c30dbc229e71edb5
Russ Wells <russ@russdigital.com>
2018-08-05 20:08:40 -0700

    loggedin test

--- src/App.js
+++ src/App.js
@@ -4,18 +4,42 @@ import phoenixred from './phoenix_red.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    loggedIn:false,
+  }
+  
   render() {
+    const loggedIn = this.state.loggedIn
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={phoenixred} className="App-logo" alt="logo" />
-          <h1 className="App-title">Sexy Awakening</h1>
-        </header>
-        <p className="App-intro">
-          Web App
-        </p>
-      </div>
-    );
+          <div className="App">
+          {loggedIn ? (
+            <div>
+              <header className="App-header">
+              <img src={phoenixred} className="App-logo" alt="logo" />
+                <h1 className="App-title">Sexy Awakening</h1>
+              </header>
+              <p className="App-intro">
+                Web App {loggedIn && (
+                  <div>holla</div>
+                )}
+              </p>
+            </div>
+          ):(
+            <div>
+              <header className="App-header">
+              <img src={phoenixred} className="App-logo" alt="logo" />
+                <h1 className="App-title">Sexy Awakening</h1>
+              </header>
+              <p className="App-intro">
+                Web App {loggedIn && (
+                  <div>holla</div>
+                )}
+              </p>
+            </div>
+          )}
+          </div>
+        )
   }
 }
 
