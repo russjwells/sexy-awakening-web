8f3b4ad46193507a7d54c577335ea08734b66b21
Russ Wells <russ@russdigital.com>
2020-01-20 19:41:13 -0800

    UserData query

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -1,9 +1,5 @@
-
-
 import React from 'react';
-
 import { auth } from '../firebase';
-
 
 const SignOutButton = () =>
   <button
--- src/components/withUserData.js
+++ src/components/withUserData.js
@@ -1,19 +1,28 @@
-import React from 'react';
-
+import React {useState} from 'react';
 import UserDataContext from './UserDataContext';
 import { firebase } from '../firebase';
+
+getUser = (uid) => {
+  return firebase.database().ref('users').child(uid).once('value')
+}
 
 const withUserData = (Component) => {
   class WithUserData extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+          authUser: null,
           userData: null,
         };
       }
     
       componentDidMount() {
+        const authUser this.state.authUser
+        const user = getUser(authUser.uid)
+        this.setState({userData: getUser(user)})
         //QUURY DATABASE FOR USER DATA AND SET LOCAL STATE 
+        //
+        //
         //REPLACEBELOW  
         //firebase.auth.onAuthStateChanged(authUser => {
         //  authUser
