957bf473774f35d60f80e2475e22f473d4a707dc
Russ Wells <russ@russdigital.com>
2020-08-27 17:13:13 -0700

    cleanup

--- src/screens/home.js
+++ src/screens/home.js
@@ -22,16 +22,7 @@ import {db} from '../firebase'
 function Home (props) {
     const {userData, setUserData} = useContext(UserDataContext)
     const [user, setUser] = useState(null)
-    /*
-    async () => {
-      const snap = await db.onceGetUserData(props.authUser.uid)
-      const usr = snap.val()
-      setUser(usr)
-    }
-    */
-    //getUserData(props.authUser.uid).then(data=>setUser(data))
-    //setUser(userData)
-    //const UserData = UserDataContext
+    
     const [profileIndex, setProfileIndex] = useState(0)
     const [profiles, setProfiles] = useState([])
     const [drawer, setDrawer] = useState(false)
@@ -78,10 +69,6 @@ function Home (props) {
         return data
         })
     }
-
-    //const loadData = async (uid) => {
-
-    //}
     
     //run did mount
     useEffect(() => {
@@ -92,149 +79,6 @@ function Home (props) {
      // setUserData(usr)
       },[userData]
     )
-      
-      
-    
-
-    /*  
-    componentWillMount() {
-        //get and set userData
-        let authUser = this.context
-        const uid = authUser.uid
-        //alert('current uid: ' + uid)
-        console.log('auth uid: ' + uid)
-        //this.setState(cuid, uid)
-        //SUCCESSFUL UID
-        // THIS WILL BE USEFUL AGAIN SOON
-        //this.updateUserLocation(uid)
-        firebase.database().ref('users').child(uid).on('value', snap => {
-          const user = snap.val()
-          this.setState({
-            userData: user,
-            profiles: [],
-            profileIndex: 0,
-          }, ()=>{
-            console.log("user data present in state:")
-            console.log(this.state.userData)
-          })
-        })
-    }
-    */
-   /*
-    componentDidMount() {
-      //get and set userData
-      
-      let authUser = this.context
-      const uid = authUser.uid
-      firebase.database().ref('users').child(uid).on('value', snap => {
-        const user = snap.val()
-        this.setState({
-          userData: user,
-          profiles: [],
-          profileIndex: 0,
-        }, ()=>{
-          console.log("data for "+this.state.userData.first_name + " is set in state")
-          console.log(this.state.userData)
-        })
-      })
-      
-    }
-    */
-    /*
-    static getDerivedStateFromProps(props, state) {
-        return {
-            user: props.user
-        }
-    }
-    */
-    /*
-      getSwiped = (uid) => {
-        
-        const swipedSex = firebase.database().ref('relationships').child(uid).child('sex').once('value').then(snap => snap.val())
-        const swipedRomance = firebase.database().ref('relationships').child(uid).child('romance').once('value').then(snap => snap.val())
-        const swipedFriendship = firebase.database().ref('relationships').child(uid).child('friendship').once('value').then(snap => snap.val())
-        const swipedPass = firebase.database().ref('relationships').child(uid).child('pass').once('value').then(snap => snap.val())
-        console.log('swiped sex', {swipedSex})
-        const allSwiped = [...swipedSex, ...swipedRomance, ...swipedFriendship, ...swipedPass]
-        console.log('all swiped', allSwiped)
-        return allSwiped || {}
-      }
-    
-      getSwipedSex = (uid) => {
-        return firebase.database().ref('relationships').child(uid).child('sex')
-          .once('value')
-          .then(snap => snap.val() || {})
-      }
-      getSwipedRomance = (uid) => {
-        return firebase.database().ref('relationships').child(uid).child('romance')
-          .once('value')
-          .then(snap => snap.val() || {})
-      }
-      getSwipedFriendship = (uid) => {
-        return firebase.database().ref('relationships').child(uid).child('friendship')
-          .once('value')
-          .then(snap => snap.val() || {})
-      }
-      getSwipedPass = (uid) => {
-        return firebase.database().ref('relationships').child(uid).child('pass')
-          .once('value')
-          .then(snap => snap.val() || {})
-      }
-
-      getProfiles = async (uid, distance) => {
-        const geoFireRef = new GeoFire(firebase.database().ref('geoData'))
-        const userLocation = await geoFireRef.get(uid)
-        //console.log('userLocation', userLocation)
-        const swipedSex = await this.getSwipedSex(uid)
-        const swipedRomance = await this.getSwipedRomance(uid)
-        const swipedFriendship = await this.getSwipedFriendship(uid)
-        const swipedPass = await this.getSwipedPass(uid)
-        const swipedProfiles = _.merge(swipedSex, swipedRomance, swipedFriendship, swipedPass)
-        
-        const geoQuery = geoFireRef.query({
-          center: userLocation,
-          radius: distance, //km
-        })
-        geoQuery.on('key_entered', async (uid, location, distance) => {
-          //console.log(uid + ' at ' + location + ' is ' + distance + 'km from the center')
-          const user = await this.getUser(uid)
-          //console.log('querying for ' + user.val().first_name)
-          const profiles = [...this.state.profiles, user.val()]
-          //console.log('profiles', profiles)
-          const filtered = filter(profiles, this.state.user, swipedProfiles)
-          this.setState({profiles: filtered})
-          //this.setState({profiles: profiles})
-        })
-      }
-      */
-     /*
-
-      updateUserLocation = async (uid) => {
-    
-        const {status} = await Permissions.askAsync(Permissions.LOCATION)
-        if (status === 'granted') {
-          const location = await Location.getCurrentPositionAsync({enableHighAccuracy: false})
-          const {latitude, longitude} = location.coords
-          const geoFireRef = new GeoFire(firebase.database().ref('geoData'))
-          geoFireRef.set(uid, [latitude, longitude])
-          console.log('Location: ', location)
-        } else {
-          console.log('Permission Denied')
-        }
-    
-        
-        //demo coords
-        //  const latitude = 37.39239
-        //  const longitude = -122.09072
-        //  const geoFireRef = new GeoFire(firebase.database().ref('geoData'))
-        //  geoFireRef.set(uid, [latitude, longitude])
-        //  console.log('temp coordinates')
-        
-        
-    
-      }
-*/
-    
     
     return (
         <AuthUserContext.Consumer>
@@ -287,7 +131,6 @@ function Home (props) {
           )}
         </AuthUserContext.Consumer>
     )
-    
 }
 //Home.contextType = AuthUserContext
 const styles = StyleSheet.create({
